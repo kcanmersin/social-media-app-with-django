ef5b9a1cdec139bd22fb9407f4518de2f2d7ff49
Kerim Can Mersin <73068663+kcanmersin@users.noreply.github.com>
2023-10-23 02:20:15 +0300

    add searching

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -1,3 +1,4 @@
+from itertools import chain
 from .models import Post, Profile ,LikePost,FollowersCount
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
@@ -11,8 +12,43 @@ def index(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile=Profile.objects(user=user_object)
 
+    user_following_list=[]
+    feed=[]
+
+    user_following =FollowersCount.objects.filter(follower=request.user.username)
+
+    for user in user_following:
+        user_following_list.append(user.user)
+    for username in user_following_list:
+        feed_list=Post.objects.filter(user=username)
+        feed.append(feed_list)
+    feed_list=list(chain(*feed))
+
     posts=Post.ojects.all()
-    return render(request,'index.html',{"user_profile":user_profile,"posts":posts})
+    
+    return render(request,'index.html',{"user_profile":user_profile,"posts":feed_list})
+@login_required(login_url='signin')
+def search(request):
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        username_object = User.objects.filter(username__icontains=username)
+
+        username_profile = []
+        username_profile_list = []
+
+        for users in username_object:
+            username_profile.append(users.id)
+
+        for ids in username_profile:
+            profile_lists = Profile.objects.filter(id_user=ids)
+            username_profile_list.append(profile_lists)
+        
+        username_profile_list = list(chain(*username_profile_list))
+    return render(request, 'search.html', {'user_profile': user_profile, 'username_profile_list': username_profile_list})
+
 @login_required(login_url='signin')
 
 def like_post(request):  
