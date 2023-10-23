ddfba817fe77b34f841a4f75407f7468775f55b1
Kerim Can Mersin <73068663+kcanmersin@users.noreply.github.com>
2023-10-23 12:28:50 +0300

    .

--- social_book/core/urls.py
+++ social_book/core/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
-from . import views 
+from . import views
 
-urlpatterns= [
+urlpatterns = [
     path('', views.index, name='index'),
     path('settings', views.settings, name='settings'),
     path('upload', views.upload, name='upload'),
-    path('like-post', views.like_post, name='like-post'),
+    path('follow', views.follow, name='follow'),
+    path('search', views.search, name='search'),
     path('profile/<str:pk>', views.profile, name='profile'),
+    path('like-post', views.like_post, name='like-post'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name='logout'),
-    path('follow', views.follow, name='follow'),
-
 ]
